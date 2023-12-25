d2ee07b9fb3d0c54198228e97f099f2674bc4e27
Franklin <esendufranklinebi@gmail.com>
2023-12-24 16:35:18 -0800

    added signup page

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ const App = () => {
       ) : (
         <>
           <Navbar />
+          
 
           <h1 className="mb-3">BEGIN YOUR TRADING AND INVESTMENT JOURNEY</h1>
           <p className="typo2 mb-3">
@@ -32,7 +33,7 @@ const App = () => {
           </p>
           <img src={Icongroup} alt="" className="mx-auto mb-3" />
 
-          <h2 className="text-black text-center font-inter text-lg font-bold tracking-wider mt-2 mb-2">
+          <h2 className=" text-black text-center font-inter text-lg font-bold tracking-wider mt-4 mb-2">
             WE PUT OUR INVESTORS FIRST
           </h2>
 
@@ -48,7 +49,7 @@ const App = () => {
             the industry.. Trade on the most comprehensive list of assets in the
             industry with our advanced web-based trading platform.. Keep your
             trading costs low with competitive spreads, commissions, and low
-            margins.. Check out spreads on our most popular cash instruments.
+            margins. Check out spreads on our most popular cash instruments.
           </p>
 
           <div className="bg-purple-100 mt-3 py-6 px-3 ">
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,107 @@
-export default function Signup() {
-    
-    return (
-        <>
+import Logo from "../assets/Ellipse 9.png"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { FormEvent } from "react"
 
-            <h1>This iis the signup page</h1>
-        </>
+export default function Signup() {
+    const [firstname,setFirstname] = useState('')
+    const [lastname,setLastname] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [confirm,setConfirm] = useState('')
+    const [country,setCountry] = useState('')
+    const [phone,setPhone] = useState('')
+    const [error,setError] = useState<string|null>();
+    const [isloading,setIsloading] = useState<boolean>()
+
+    const confirmPassword = (a:string,b:string) =>{
+        if(a!==b){
+            setError("Passwords do not match")
+            throw  Error
+        }else{
+            return
+        }
+
+    }
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        setIsloading(true)
+        confirmPassword(password,confirm)
+
+    }
+    return (
+        <div className="p-4">
+        <nav className="flex justify-between mb-4 sticky top-0 px-3 p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+</svg>
+
+
+
+
+<div className="flex w-4/5 ">
+                <img src={Logo} className=" scale-50 " width={30} alt="" />
+                <header className="font-bold tracking-wide">CRYPTONETVERSE</header>
+          
+            </div>
+            <div></div>
+
+
+        </nav>
+
+        <main>
+            <h1 style={{fontStyle:'bold',textShadow:'none',fontSize:'1rem',marginBottom:'3rem'}}className="text-bold mb-5 shadow-none">SIGNUP</h1>
+            {error && <div  className="text-red-500">{error}</div>}
+            <form onSubmit={handleSubmit} className="mt-5">
+
+                <div className="flex gap-x-2 mb-4">
+
+
+                <div className="w-3/6">
+
+                <label>Firstname</label>
+                <Input className="  rounded border-2" type='text' value={firstname} onChange={(e)=>setFirstname(e.target.value)} required/>
+                </div>
+
+                
+                <div className="w-3/6">
+
+                <label>Lastname</label>
+                <Input className=" p-2 rounded border-2"  type="text" value={lastname} onChange={(e)=>setLastname(e.target.value)} required/>
+                </div>
+                </div>
+
+
+                <div className="mb-4">
+                    <label htmlFor="email">Email address</label>
+                    <Input className="rounded border-2" type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password">Password</label>
+                    <Input className="rounded border-2" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="Confirm password">Confirm Password</label>
+                    <Input className="rounded border-2" type='text' value={confirm} onChange={(e)=>setConfirm(e.target.value)} required/>
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="Country">Country</label>
+                    <Input className="rounded border-2"type="text" value={country}  onChange={(e)=>setCountry(e.target.value)} required/>
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="Phone">Phone</label>
+                    <Input className="rounded border-2" type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} required/>
+                </div>
+                
+
+            <Button disabled={isloading} className="bg-blue-500 text-white w-3/5 mx-auto mt-3" type="submit">SUBMIT</Button>
+            </form>
+
+        </main>
+
+
+        </div>
     )
 }
